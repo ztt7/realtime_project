ae4e5fcfbdd12f08e8ba0781235f40aaec868c7e
ztt <ztt@163.com>
2024-05-10 17:54:45 +0800

    5.10

---
+++ batch_jobs/src/main/java/cn/doitedu/batch/jobs/DataLoadJob01_GeoAreaInfo2Hbase.scala
@@ -0,0 +1,90 @@
+package cn.doitedu.batch.jobs
+
+import ch.hsr.geohash.GeoHash
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.spark.sql.SparkSession
+
+import java.util.Properties
+
+object DataLoadJob01_GeoAreaInfo2Hbase {
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder()
+      .appName("gps地域维表etl任务")
+      .master("local")
+      .getOrCreate()
+
+    val props = new Properties()
+    props.setProperty("user","root")
+    props.setProperty("password","root")
+
+    val dataFrame = sparkSession.read.jdbc("jdbc:mysql://doitedu:3306/realtimedw", "t_md_areas", props)
+
+    dataFrame.createTempView("t")
+
+    val reverseGeoFunc = (lat:Double, lng:Double)=>{
+      GeoHash.geoHashStringWithCharacterPrecision(lat,lng,5).reverse
+    }
+
+    sparkSession.udf.register("geo",reverseGeoFunc)
+
+    val resDf = sparkSession.sql(
+      """
+        |select
+        |   geohash,
+        |   province,
+        |   city,
+        |   nvl(region,'') as region
+        |from(
+        |   select
+        |      geohash,
+        |      province,
+        |      city,
+        |      region,
+        |      -- 利用row_number()over() 对 相同重复的数据 进行去重
+        |      row_number() over(partition by geohash order by province) as rn
+        |   from
+        |   (
+        |      -- 对原始地理位置表，进行自关联，将层级数据扁平化
+        |      SELECT
+        |        geo(lv4.BD09_LAT, lv4.BD09_LNG) as geohash,
+        |        lv1.AREANAME as province,
+        |        lv2.AREANAME as city,
+        |        lv3.AREANAME as region
+        |      from t lv4
+        |        join t lv3 on lv4.`LEVEL`=4 and lv4.bd09_lat is not null and lv4.bd09_lng is not null and lv4.PARENTID = lv3.ID
+        |        join t lv2 on lv3.PARENTID = lv2.ID
+        |        join t lv1 on lv2.PARENTID = lv1.ID
+        |   ) o1
+        |) o2
+        |
+        |where rn=1 and geohash is not null and length(trim(geohash))=5
+        |and province is not null and trim(province)!=''
+        |and city is not null and trim(city)!=''
+        |
+        |""".stripMargin)
+
+    // 将加工好的数据，写入hbase
+    resDf.rdd.map(row => {
+      val geoHashCode = row.getAs[String]("geohash")
+      val province = row.getAs[String]("province")
+      val city = row.getAs[String]("city")
+      val region = row.getAs[String]("region")
+
+      // 将本行数据封装到hbase的数据插入对象Put中
+      val put = new Put(geoHashCode.getBytes())
+      put.addColumn("f".getBytes(), "province".getBytes(), province.getBytes())
+      put.addColumn("f".getBytes(), "city".getBytes(), city.getBytes())
+      put.addColumn("f".getBytes(), "region".getBytes(), region.getBytes())
+
+      //new ImmutableBytesWritable() 、new Put() 是hbase里面的一个类型
+            (new ImmutableBytesWritable(), put)
+          }).saveAsNewAPIHadoopDataset(job.getConfiguration)
+
+          sparkSession.close()
+
+    }
+
+
+
+}
--- realtime_dw/src/main/java/cn/doitedu/rtdw/data_sync/SyncJob01_UserInfo2Hbase.java
+++ realtime_dw/src/main/java/cn/doitedu/rtdw/data_sync/SyncJob01_UserInfo2Hbase.java
@@ -18,7 +18,49 @@ public class SyncJob01_UserInfo2Hbase {
         // 构造table编程环境
         StreamTableEnvironment tenv = StreamTableEnvironment.create(env);
 
+        /**
+         * 监听MySQL的binlog，得到 用户注册信息表数据，然后写入  hbase
+         */
 
-        
+        // 建连接器表，映射  mysql中的 用户注册信息表<ums_member>
+        // 挑选一些关心的维度字段：  id,username,phone,status,create_time,gender,province,city,job
+        tenv.executeSql("CREATE TABLE ums_member_source (    " +
+                "      id BIGINT,                                   " +
+                "      username STRING,                             " +
+                "      phone STRING,                                " +
+                "      status int,                                  " +
+                "      create_time timestamp(3),                    " +
+                "      gender int,                                  " +
+                "      birthday date,                               " +
+                "      province STRING,                             " +
+                "      city STRING,                                 " +
+                "      job STRING ,                                 " +
+                "      source_type INT ,                            " +
+                "     PRIMARY KEY (id) NOT ENFORCED            " +
+                "     ) WITH (                                 " +
+                "     'connector' = 'mysql-cdc',               " +
+                "     'hostname' = 'doitedu'   ,               " +
+                "     'port' = '3306'          ,               " +
+                "     'username' = 'root'      ,               " +
+                "     'password' = 'root'      ,               " +
+                "     'database-name' = 'realtimedw',          " +
+                "     'table-name' = 'ums_member'              " +
+                ")");
+
+
+        // 建连接器表，映射  hbase中的 用户信息维表<dim_user_info>
+        tenv.executeSql("CREATE TABLE ums_member_hbasesink( " +
+                " username STRING, " +
+                " f ROW<id BIGINT,phone STRING, status INT, create_time TIMESTAMP(3), gender INT, birthday DATE, province STRING, city STRING, job STRING, source_type INT>, " +
+                " PRIMARY KEY (username) NOT ENFORCED " +
+                ") WITH (                             " +
+                " 'connector' = 'hbase-2.2',          " +
+                " 'table-name' = 'dim_user_info',     " +
+                " 'zookeeper.quorum' = 'doitedu:2181' " +
+                ")");
+
+        // 写一个insert ... select  from ..  的 sql
+        tenv.executeSql("insert into ums_member_hbasesink select username,row(id,phone,status,create_time,gender,birthday,province,city,job,source_type) as f from ums_member_source");
+
     }
 }
